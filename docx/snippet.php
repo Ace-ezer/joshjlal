3a8b89f2c98e67868cd66381466c92d1521dc927
Arpit Yadav <34399051+Ace-ezer@users.noreply.github.com>
2019-01-10 18:31:21 +0530

    Add files via upload

---
+++ docx/snippet.php
@@ -0,0 +1,18 @@
+<?php
+	
+	require_once 'server.php';
+
+
+	$sql="SELECT * FROM events WHERE event='volleyball';";
+	$res=mysqli_query($conn,$sql);
+    $row=mysqli_fetch_assoc($res);
+
+    if($row['set1']!=0)
+	echo	"<a href='documents/volleyballset1.pdf'>Schedule</a>";
+	else  echo	"<a href='#''>Schedule</a>";
+	if($row['set2']!=0)
+	echo	" <a href='documents/volleyballset2.pdf'>Results</a>";
+	else   echo "<a href='#''>Result</a>";
+  	echo "<a href='#''>Rules</a>";
+?>
+
---
+++ upload.php
@@ -0,0 +1,55 @@
+<?php
+    
+    session_start();
+    require_once 'server.php';
+
+	$_SESSION["output"]="";
+	$file=$name=$stat=$event="";
+
+	$name=$_SESSION['game'];
+	$event=$_SESSION['game'];
+
+if(isset($_POST['submit']))
+	{
+		$file=$_FILES['file'];
+		$stat='set1';
+	}
+	else if(isset($_POST['submit1'])){
+		$file=$_FILES['file1'];
+		$stat='set2';
+	}
+
+
+	if(!empty($file)){
+
+		$filename=$file['name'];
+		$filetmpname=$file['tmp_name'];
+		$type=$file['type'];
+		$error=$file['error'];
+		$size=$file['size'];
+
+		$fileExt=explode('.', $filename);
+     	$fileActualExt=strtolower(end($fileExt));
+
+     	$allowed= array('pdf','docx','jpg','png','jpeg');
+
+     	if(in_array($fileActualExt, $allowed)){
+     		if($error==0){
+
+     			if($size<1000000){
+     				$filenamenew=$name.$stat.".".$fileActualExt;
+     				$fileDestination='documents/'.$filenamenew;
+     				move_uploaded_file($filetmpname, $fileDestination);
+     				$sql="UPDATE events SET ".$stat."=1 WHERE event='".$event."';";
+					$res=mysqli_query($conn,$sql);
+					$_SESSION['output']="File Uploaded!";
+					header('location:upload_docx.php?success');
+     			}
+     			else echo 'File size too Big.';
+     		}
+     		else echo 'There was an error while uploading your file.';
+     	}
+     	else echo 'File format not Supported.';
+	}
+
+?>
